70f440cc034cd5ea4bdeea6893b345e4a3472b8f
xinzhang <xinchang@greatonce.com>
2019-12-19 18:12:30 +0800

    初始化项目

---
+++ xinzhang-consumer/src/main/java/top/xinzhang0618/consumer/ConsumerApplication.java
@@ -0,0 +1,19 @@
+package top.xinzhang0618.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ConsumerApplication
+ *
+ * @author xinzhang
+ * @author Shenzhen Greatonce Co Ltd
+ * @version 2019/12/19
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerApplication.class, args);
+  }
+}
---
+++ xinzhang-producer/src/main/java/top/xinzhang0618/producer/ProducerApplication.java
@@ -0,0 +1,19 @@
+package top.xinzhang0618.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ProducerApplication
+ *
+ * @author xinzhang
+ * @author Shenzhen Greatonce Co Ltd
+ * @version 2019/12/19
+ */
+@SpringBootApplication
+public class ProducerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProducerApplication.class, args);
+  }
+}
